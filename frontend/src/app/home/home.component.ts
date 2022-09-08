47f1373aee3c295bb52c195dc49b46b814ad0a58
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-09-08 16:44:11 +0200

    main panel text and animations

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,14 +3,18 @@ import { Component, OnInit } from '@angular/core';
 
 const fadeIn = trigger('fadeIn', [
   state('in', style({ opacity: 1 })),
-  transition('void => *', [style({ opacity: 0}), animate('1s ease-in')]),
-])
+  transition('void => *', [style({ opacity: 0}), animate('1.3s ease-out')]),
+]);
 
+const lettersIn = trigger('lettersIn', [
+  state('in', style({ opacity: 1, 'letter-spacing': 'normal' })),
+  transition('void => *', [style({ opacity: 0, 'letter-spacing': '15px'}), animate('1.2s ease-out')]),
+]);
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [fadeIn]
+  animations: [fadeIn, lettersIn]
 })
 export class HomeComponent implements OnInit {
 
