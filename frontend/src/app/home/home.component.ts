513866802e4de15b6e0e06dc8b68a4b186d52c24
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-09-08 11:24:07 +0200

    overlay animation

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,16 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+const fadeIn = trigger('fadeIn', [
+  state('in', style({ opacity: 1 })),
+  transition('void => *', [style({ opacity: 0}), animate('1s ease-in')]),
+])
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeIn]
 })
 export class HomeComponent implements OnInit {
 
