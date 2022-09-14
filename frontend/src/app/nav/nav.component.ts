967cb97c5a034aadd72a300b95cfae775576120d
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-09-14 17:29:50 +0200

    navbar scrolling animation

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../user/login-dialog/login-dialog.component';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-nav',
@@ -9,8 +13,32 @@ import { LoginDialogComponent } from '../user/login-dialog/login-dialog.componen
 })
 export class NavComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
+  public dialogOpen: boolean = false;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initScrollingAnimations();
+  }
+
+  initScrollingAnimations() {
+
+    const showNav = gsap.fromTo('.navbar', { 
+      opacity: 0,
+      visibility: 'hidden',
+      // paused: true,
+    }, {
+      visibility: 'visible',
+      opacity: 1,
+      duration: 0.4
+    });
+    
+    ScrollTrigger.create({
+      start: "top top",
+      end: 99999,
+      onUpdate: (self) => {
+        self.direction === -1 ? showNav.play() :  showNav.reverse()
+      }
+    });
+  }
 
   openLoginPanel() {
     let dialogRef = this.dialog.open(LoginDialogComponent, {
@@ -18,8 +46,14 @@ export class NavComponent implements OnInit {
       panelClass: 'my-custom-dialog-class'
     });
 
+    dialogRef.afterOpened().subscribe(result => {
+      console.log('The dialog was opened');
+      this.dialogOpen = true;
+    });
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
+      this.dialogOpen = false;
     });    
   }
 }
--- frontend/src/app/shared/directives/scroll-position.directive.ts
+++ frontend/src/app/shared/directives/scroll-position.directive.ts
@@ -4,16 +4,23 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
   selector: '[appScrollPosition]',
 })
 export class ScrollPositionDirective {
+  previousPosition: number | null = null;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event: Event) {
-    const position = (event.target as Document).scrollingElement?.scrollTop;
+    // console.log(event)
+    const position: number = (event.target as Document).scrollingElement?.scrollTop as number;
 
     if(position === 0) {
       this.renderer.addClass(this.element.nativeElement, 'navbar-transparent');
     } else {
+      if (this.previousPosition !== null && this.previousPosition > position) {
       this.renderer.removeClass(this.element.nativeElement, 'navbar-transparent');
     }
+
+    this.previousPosition = position;
   }
+}
 }
