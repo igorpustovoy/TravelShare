351a2ea48db80a0ee1540ff75dbbb743b45b0e72
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-09-14 13:55:11 +0200

    lower footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { PhotoPanelComponent } from './home/photo-panel/photo-panel.component';
 import { FooterComponent } from './footer/footer.component';
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserAnimationsModule,
     MatDialogModule,
     MatIconModule,
-    
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
